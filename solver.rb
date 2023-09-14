3679e8e1f63f9f44b99ebac9d90bbfced5bf7384
Henok Mekonnen <henokmekonnen5@gmail.com>
2023-09-14 23:25:21 +0300

    linter error fix

--- solver.rb
+++ solver.rb
@@ -1,17 +1,17 @@
 class Solver
-    def factorial(number)
-        raise 'This method only accepts positive numbers' if number.negative?
-    
-        result = 1
-        current_n = 2
-    
-        while current_n <= number
-          result *= current_n
-          current_n += 1
-        end
-    
-        result
-      end
+  def factorial(number)
+    raise 'This method only accepts positive numbers' if number.negative?
+
+    result = 1
+    current_n = 2
+
+    while current_n <= number
+      result *= current_n
+      current_n += 1
+    end
+
+    result
+  end
       def reverse(word)
         word.reverse
       end
