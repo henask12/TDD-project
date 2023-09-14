c6d27eef78d0cd6212e2f9540ad16bac46bd0ed4
Henok Mekonnen <henokmekonnen5@gmail.com>
2023-09-14 23:30:23 +0300

    factorial test file creation

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,4 +1,14 @@
 require_relative '../solver'
 
 describe Solver do
+  subject { Solver.new }
+
+  it 'should return the factorial of 6' do
+    expect(subject.factorial(6)).to eq(720)
+  end
+
+  it 'should return the factorial of 5' do
+    expect(subject.factorial(5)).to eq(120)
+  end
+
 end
