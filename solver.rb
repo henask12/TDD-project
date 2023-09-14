6a10ea42ebf13ea695dc9ba87e1c16134742ea9b
Henok Mekonnen <henokmekonnen5@gmail.com>
2023-09-14 23:26:27 +0300

    linter error fix

--- solver.rb
+++ solver.rb
@@ -12,9 +12,10 @@ class Solver
 
     result
   end
-      def reverse(word)
-        word.reverse
-      end
+
+  def reverse(word)
+    word.reverse
+  end
       def fizzbuzz(num)
         if (num % 3).zero? && (num % 5).zero?
           'fizzbuzz'
